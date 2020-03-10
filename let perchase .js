376db6975b01eb01e49bc895af793ff7786440bd
mollyham <nakazibwemolly@gmail.com>
2020-03-10 13:38:40 +0300

    my first css fil

---
+++ let perchase .js
@@ -0,0 +1,26 @@
+let priceTag = 30;
+let money = 25;
+ if (priceTag==money){
+     console.log("purchasse item");
+
+ }
+
+ else {
+     console.log("dont purchase item");
+ }
+
+
+let weather =prompt("what is the weather like today?")
+
+if (weather== "sunny") {console.log ("wear vest.");
+
+}
+else if (weather =="cloudy"){
+    console.log("wear a rain coat.");
+}
+else if( weather == "windy"){
+    console.log("wear a scaff");
+}
+else{
+    console.log("Naked");
+}
